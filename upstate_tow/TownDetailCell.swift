6f4f9fa04242baf1c34f62d7bce4501701a6251d
Jose Carlos <josealarconchacon@gmail.com>
2020-09-17 14:05:22 -0400

    Build A Slide In Transition

---
+++ upstate_tow/TownDetailCell.swift
@@ -0,0 +1,24 @@
+//
+//  TownDetailCell.swift
+//  upstate_tow
+//
+//  Created by Jose Alarcon Chacon on 9/16/20.
+//  Copyright © 2020 Jose Alarcon Chacon. All rights reserved.
+//
+
+import UIKit
+
+class TownDetailCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

3e9d5746d8a2f244250bab874e5682b1e99e5c5d
Jose Carlos <josealarconchacon@gmail.com>
2020-09-17 22:55:54 -0400

    move file to Extention folder

---
+++ upstate_tow/Extention/Imagelayout.swift
@@ -0,0 +1,36 @@
+//
+//  Imagelayout.swift
+//  upstate_tow
+//
+//  Created by Jose Alarcon Chacon on 9/15/20.
+//  Copyright © 2020 Jose Alarcon Chacon. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+
+extension UIImageView {
+     func image_layout() {
+        layer.cornerRadius = 15.0
+    }
+    
+    func round_image() {
+        self.layer.cornerRadius = (self.frame.width / 2) //instead of let radius = CGRectGetWidth(self.frame) / 2
+        self.layer.masksToBounds = true
+    }
+    
+    func town_imag_detail() {
+        clipsToBounds = true
+        layer.cornerRadius = 20
+        layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
+    }
+}
+
+extension MKMapView {
+    func map_layout() {
+        self.layer.cornerRadius = (self.frame.width / 2)
+        self.layer.masksToBounds = true
+    }
+}
+
---
+++ upstate_tow/Extention/SideMenu.swift
@@ -0,0 +1,16 @@
+//
+//  SideMenu.swift
+//  upstate_tow
+//
+//  Created by Jose Alarcon Chacon on 9/16/20.
+//  Copyright © 2020 Jose Alarcon Chacon. All rights reserved.
+//
+
+import Foundation
+
+enum SideMenu: Int {
+    case town
+    case edit_profile
+    case setting
+}
+
